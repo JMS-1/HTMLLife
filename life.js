8c16949c10a3d8cc7258e20f5ad8fd03cabfcfbb
JMS-1 <dev-2@psimarron.net>
2015-04-24 22:41:01 +0200

    More pure JavaScript code - back to the roots :-(

--- life.js
+++ life.js
@@ -1,17 +1,20 @@
-﻿window.onload = function () {
+﻿// Jede Zelle erhält die Information, ob sie lebt oder tot ist
+var cssLebt = 'lebt';
 
-    // Jede Zelle erhält die Information, ob sie lebt oder tot ist
-    var nameZustand = 'data-zustand';
-    var zustandLebt = 'lebt';
+// Die folgenden Variablen werden gesetzt, sobald die Seite geladen ist
+var schritteInfo, intervall, alleZellen, zeilen, spalten
+
+// Diese Methode wird Aufgerufen, wenn die HTML Seite komplett geladen ist
+function seiteGeladen() {
 
     // Die wichtigsten visuellen Elemente ermitteln
-    var container = document.querySelector('#spielfeld tbody');
-    var schritteInfo = document.getElementById('schritte');
-    var interval = document.getElementById('interval');
+    schritteInfo = document.getElementById('schritte');
+    intervall = document.getElementById('interval');
 
-    // Die Größe des Spielfelds auslesen
-    var zeilen = parseInt(container.getAttribute('data-zeilen'));
-    var spalten = parseInt(container.getAttribute('data-spalten'));
+    // Die Spielfeld und dessen Größe auslesen
+    var spielfeld = document.querySelector('#spielfeld tbody');
+    zeilen = parseInt(spielfeld.getAttribute('data-zeilen'));
+    spalten = parseInt(spielfeld.getAttribute('data-spalten'));
 
     // Tabelle zusammenbauen
     var html = '';
@@ -20,123 +23,125 @@
         html += '<tr>';
 
         for (var s = 0; s < spalten; s++)
-            html += '<td onclick="zelleUmschalten(this)" ' + nameZustand + '=""/>';
+            html += '<td onclick="zelleUmschalten()" />';
 
         html += '</tr>';
     }
 
-    container.innerHTML = html;
+    // HTML Elemente auf einen Rutsch erzeugen lassen - das geht am schnellsten
+    spielfeld.innerHTML = html;
 
     // Hier merken wir uns alle Zellen
-    var alleZellen = container.querySelectorAll('td');
-
-    var start = new Date().getTime();
-    var timerOn = false;
-    var schritte = 0;
-
-    // Anzahl der Schritte anzeigen
-    function schritteAnzeigen() {
-        var delta = new Date().getTime() - start;
-        var proSekunde = (delta > 0) ? Math.round(1000 * schritte / delta) : 0;
-
-        // Anzeigen
-        schritteInfo.textContent = proSekunde;
-
-        // Neu aufsetzen
-        start = new Date().getTime();
-        schritte = 0;
-
-        // Später noch einmal
-        window.setTimeout(schritteAnzeigen, 1000);
-    }
+    alleZellen = spielfeld.querySelectorAll('td');
 
     // Anzeige erstmalig vornehmen
     schritteAnzeigen();
+};
 
-    // Hier wird die nächste Generation berechnet
-    window.nächsteGeneration = function () {
-        schritte = schritte + 1;
+// Die Daten für die Statistik
+var start = new Date().getTime();
+var automatisch = false;
+var schritte = 0;
 
-        // Zuerst einmal schauen wir uns den IST Zustand an
-        var vorher = [];
-        for (var zs = 0; zs < alleZellen.length; zs++) {
-            var zelle = alleZellen[zs];
+// Hier wird die nächste Generation berechnet
+function naechsteGeneration() {
+    schritte = schritte + 1;
 
-            vorher.push({
-                lebt: (zelle.getAttribute(nameZustand) == zustandLebt),
-                zeile: Math.floor(zs / spalten),
-                spalte: zs % spalten,
-                zelle: zelle,
-                nachbarn: 0,
-            });
-        };
+    // Zuerst einmal schauen wir uns den IST Zustand an
+    var vorher = [];
+    for (var zs = 0; zs < alleZellen.length; zs++) {
+        var zelle = alleZellen[zs];
 
-        // Jede lebende Zelle trägt sich bei allen Nachbarn ein
-        for (var zs = 0; zs < vorher.length; zs++) {
-            var zelle = vorher[zs];
+        vorher.push({
+            lebt: (zelle.className == cssLebt),
+            zeile: Math.floor(zs / spalten),
+            spalte: zs % spalten,
+            zelle: zelle,
+            nachbarn: 0,
+        });
+    };
 
+    // Jede lebende Zelle trägt sich bei allen Nachbarn ein
+    for (var zs = 0; zs < vorher.length; zs++) {
+        var zelle = vorher[zs];
+
+        if (!zelle.lebt)
+            continue;
+
+        for (var z = Math.max(0, zelle.zeile - 1), zm = Math.min(zeilen, zelle.zeile + 2) ; z < zm; z++)
+            for (var s = Math.max(0, zelle.spalte - 1), sm = Math.min(zeilen, zelle.spalte + 2) ; s < sm; s++)
+                vorher[s + z * spalten].nachbarn += 1;
+
+        // Die einfache Schleife macht jede Zelle zu ihrem eigenen Nachbarn, das müssen wir natürlich rückgängig machen
+        zelle.nachbarn -= 1;
+    };
+
+    // Und nun nur noch abhängig von der Anzahl der Nachbarn den Zustand der Zelle verändern - oder belassen, wie er ist
+    for (var zs = 0; zs < vorher.length; zs++) {
+        var zelle = vorher[zs];
+
+        if (zelle.nachbarn == 3) {
+            // Nur ändern wenn nötig
             if (!zelle.lebt)
-                continue;
-
-            for (var z = Math.max(0, zelle.zeile - 1), zm = Math.min(zeilen, zelle.zeile + 2) ; z < zm; z++)
-                for (var s = Math.max(0, zelle.spalte - 1), sm = Math.min(zeilen, zelle.spalte + 2) ; s < sm; s++)
-                    vorher[s + z * spalten].nachbarn += 1;
-
-            // Die einfache Schleife macht jede Zelle zu ihrem eigenen Nachbarn, das müssen wir natürlich rückgängig machen
-            zelle.nachbarn -= 1;
-        };
-
-        // Und nun nur noch abhängig von der Anzahl der Nachbarn den Zustand der Zelle verändern - oder belassen, wie er ist
-        for (var zs = 0; zs < vorher.length; zs++) {
-            var zelle = vorher[zs];
-
-            if (zelle.nachbarn == 3) {
-                // Nur ändern wenn nötig
-                if (!zelle.lebt)
-                    zelle.zelle.setAttribute(nameZustand, zustandLebt);
-            }
-            else if (zelle.nachbarn != 2) {
-                // Nur ändern wenn nötig
-                if (zelle.lebt)
-                    zelle.zelle.setAttribute(nameZustand, '');
-            }
-        };
-
-        if (!timerOn)
-            return;
-
-        // Auf Wunsch des Anwenders automatisch die nächste Generation berechnen
-        window.setTimeout(nächsteGeneration, 1000 / Math.max(1, Math.min(1000, interval.value)));
-    }
-
-    // Das Spielfeld zufällig füllen
-    window.spielfeldFüllen = function () {
-        for (var zs = 0; zs < alleZellen.length; zs++) {
-            var zelle = alleZellen[zs];
-            var zustand = (Math.random() <= 0.3) ? zustandLebt : '';
-
-            zelle.setAttribute(nameZustand, zustand);
-        };
+                zelle.zelle.className = cssLebt;
+        }
+        else if (zelle.nachbarn != 2) {
+            // Nur ändern wenn nötig
+            if (zelle.lebt)
+                zelle.zelle.className = null;
+        }
     };
 
-    // Das Spielfeld löschen
-    window.spielfeldLöschen = function () {
-        for (var zs = 0; zs < alleZellen.length; zs++) {
-            alleZellen[zs].setAttribute(nameZustand, '');
-        };
+    // Auf Wunsch des Anwenders automatisch die nächste Generation berechnen
+    if (automatisch)
+        window.setTimeout(naechsteGeneration, 1000 / Math.max(1, Math.min(1000, intervall.value)));
+}
+
+// Anzahl der Schritte anzeigen
+function schritteAnzeigen() {
+    var delta = new Date().getTime() - start;
+    var proSekunde = (delta > 0) ? Math.round(1000 * schritte / delta) : 0;
+
+    // Anzeigen
+    schritteInfo.textContent = proSekunde;
+
+    // Neu aufsetzen
+    start = new Date().getTime();
+    schritte = 0;
+
+    // Später noch einmal
+    window.setTimeout(schritteAnzeigen, 1000);
+}
+
+// Wenn eine Zelle angeklickt wird ändert sie ihren Zustand
+function zelleUmschalten() {
+    var zelle = event.srcElement;
+    var zustand = (zelle.className == cssLebt) ? null : cssLebt;
+
+    zelle.className = zustand;
+}
+
+// Das Spielfeld löschen
+function spielfeldLoeschen() {
+    for (var zs = 0; zs < alleZellen.length; zs++) {
+        alleZellen[zs].className = null;
     };
+};
 
-    // Das ständige Berechnen von Generationen akivieren
-    window.laufenLassenUmschalten = function () {
-        timerOn = !timerOn;
-        nächsteGeneration();
+// Das Spielfeld zufällig füllen
+function spielfeldFuellen() {
+    for (var zs = 0; zs < alleZellen.length; zs++) {
+        var zelle = alleZellen[zs];
+        var zustand = (Math.random() <= 0.3) ? cssLebt : null;
+
+        zelle.className = zustand;
     };
+};
 
-    // Wenn eine Zelle angeklickt wird ändert sie ihren Zustand
-    window.zelleUmschalten = function (zelle) {
-        var zustand = (zelle.getAttribute(nameZustand) == zustandLebt) ? '' : zustandLebt;
+// Das ständige Berechnen von Generationen akivieren
+function laufenLassenUmschalten() {
+    automatisch = !automatisch;
 
-        zelle.setAttribute(nameZustand, zustand);
-    }
-
+    if (automatisch)
+        naechsteGeneration();
 };
